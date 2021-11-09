f2c6862f1e07a2c483dcc6e46de744bb79d3f87a
Sumanth Sanathi <53118143+c4tbrilliantthoughts@users.noreply.github.com>
2021-11-09 08:34:29 +0530

    Update index.js
    updated footer page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,17 @@ const Footer = () => {
   return (
     <div className="footer">
       ABook.
-      <span style={{ color: "#7c7c7c" }}> Designed by Sumanth Sanathi</span>
+      <span style={{ color: "#7c7c7c" }}>
+        {" "}
+        Designed by Sumanth Sanathi &nbsp; |
+      </span>
+      <a
+        href="https://github.com/c4tbrilliantthoughts/ABook"
+        target="_blank"
+        style={{ color: "#eb641a" }}
+      >
+        &nbsp; GitHub
+      </a>
     </div>
   );
 };
